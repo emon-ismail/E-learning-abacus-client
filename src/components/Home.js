25dd4d97ebc8219f2a3317a24c89d591edfce680
Tonmoy-Roy <emonismail69@gmail.com>
2022-10-25 16:37:59 +0600

    route chnage

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,28 +18,28 @@ const Home = () => {
     {/* Carrousal */}
     <div className="carousel w-full">
   <div id="slide1" className="carousel-item relative w-full">
-    <img src="https://placeimg.com/800/200/arch" className="w-full" />
+    <img src="abacus.png" className="w-full" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide4" className="btn btn-circle">❮</a> 
       <a href="#slide2" className="btn btn-circle">❯</a>
     </div>
   </div> 
   <div id="slide2" className="carousel-item relative w-full">
-    <img src="https://placeimg.com/800/200/arch" className="w-full" />
+    <img src="abacus.png" className="w-full" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide1" className="btn btn-circle">❮</a> 
       <a href="#slide3" className="btn btn-circle">❯</a>
     </div>
   </div> 
   <div id="slide3" className="carousel-item relative w-full">
-    <img src="https://placeimg.com/800/200/arch" className="w-full" />
+    <img src="abacus.png" className="w-full" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide2" className="btn btn-circle">❮</a> 
       <a href="#slide4" className="btn btn-circle">❯</a>
     </div>
   </div> 
   <div id="slide4" className="carousel-item relative w-full">
-    <img src="https://placeimg.com/800/200/arch" className="w-full" />
+    <img src="abacus.png" className="w-full" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide3" className="btn btn-circle">❮</a> 
       <a href="#slide1" className="btn btn-circle">❯</a>
@@ -63,7 +63,7 @@ const Home = () => {
                 <Link to='/login'>
                   <button
                     type='button'
-                    className='px-8 py-3 m-20 text-lg font-semibold rounded bg-gray-800 hover:bg-gray-700 text-gray-50'
+                    className='px-8 py-3 m-2 text-lg font-semibold rounded bg-gray-800 hover:bg-gray-700 text-gray-50'
                   >
                     Login
                   </button>
@@ -71,7 +71,7 @@ const Home = () => {
                 <Link to='/register'>
                   <button
                     type='button'
-                    className='px-8 py-3 my-20 text-lg border rounded border-gray-700 text-gray-900'
+                    className='px-8 py-3 m-2 text-lg border rounded border-gray-700 text-gray-900'
                   >
                     Register
                   </button>
